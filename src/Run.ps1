b366f5a70f336b4b073ee11bd0114f0c08e70719
Jai Chatterjee <jai.chatterjee17@gmail.com>
2026-01-13 01:58:10 +0530

    Add application lifecycle scripts for run

--- src/Run.ps1
+++ src/Run.ps1
@@ -1,1 +1,6 @@
+$path = "C:\Program Files\SalesforceE2C"
 
+Start-Process pwsh.exe `
+    -ArgumentList "-STA -ExecutionPolicy Bypass -File `"$InstallPath\Main.ps1`"" `
+    -WorkingDirectory $InstallPath `
+    -WindowStyle Hidden
